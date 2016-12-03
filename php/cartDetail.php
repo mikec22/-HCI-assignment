83dd9c3b6b60c5856cd1e7a7b88d10d05a191bff
Yiniy Chung <nanasemaru@doiyasan.local>
2016-12-03 23:54:53 +0800

    cart detail

--- php/cartDetail.php
+++ php/cartDetail.php
@@ -1,7 +1,8 @@
 <?
 session_start();
 if (isset($_SESSION['cart_details']) && count($_SESSION['cart_details'])>0) {
-    echo "<div class=\"table-responsive\">
+    echo "<h2>Shopping Cart</h2>
+            <div class=\"table-responsive\">
             <table class=\"table table-striped\">
                 <thead class=\"thead-inverse\">
                     <tr>
@@ -14,18 +15,25 @@ if (isset($_SESSION['cart_details']) && count($_SESSION['cart_details'])>0) {
                     </tr>
                 </thead>
                 <tbody>";
+    $total = 0;
     foreach ($_SESSION['cart_details'] as $key => $value) {
-        $price = $value['price'];
+        $unit = $value['price'];
         $quantity = $value['quantity'];
-        $total = $price * $quantity;
+        $price = $unit * $quantity;
+        $total += $price;
         echo "<tr>
                 <td class=\"align-middle\"><img src=\"img/{$value['img']}\" style=\"height:70px; width:70px; object-fit: cover; object-position: center;\"></td>
                 <td class=\"align-middle\">{$value['name']}</td>
-                <td class=\"align-middle\">$$price</td>
-                <td class=\"align-middle\">$quantity</td>
-                <th class=\"align-middle\" scope=\"row\">$$total</th>
-                <td class=\"align-middle\"><a href=\"\"><img src=\"img/delete.png\" style=\"max-height:50px;\" onclick=\"delFromCart($key)\"></a></td>
+                <td class=\"align-middle text-xs-center\">$$unit</td>
+                <td class=\"align-middle text-xs-center\">$quantity</td>
+                <th class=\"align-middle text-xs-right\" scope=\"row\">$$price</th>
+                <td class=\"align-middle text-xs-center\"><a href=\"\"><img src=\"img/delete.png\" style=\"max-height:50px;\" onclick=\"delFromCart($key)\"></a></td>
             </tr>";
     }
+    echo "<th colspan=\"5\" scope=\"row\" class=\"align-middle text-xs-right table-info h3\">Total Price: $$total</th>
+            <td class=\"align-middle table-info text-xs-right\"><button class=\"btn btn-primary btn-block\">Checkout</button></td>
+            </tbody>
+            </table>
+        </div>";
 }
 ?>
