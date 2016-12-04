d45bc506109f24afb95c6d8f844e73bf7beb3d01
Yiniy Chung <nanasemaru@doiyasan.local>
2016-12-04 21:10:52 +0800

    .

--- php/cart.php
+++ php/cart.php
@@ -24,17 +24,23 @@ if ($action === "add") {
 if (isset($_SESSION['cart_details']) && count($_SESSION['cart_details'])>0) {
     $i = 0;
     foreach ($_SESSION['cart_details'] as $key => $value) {
+        $price = $value['price'];
+        $vipPrice = $price * 0.9;
         echo "<li>
             <span class=\"item\">
                 <span class=\"item-left\">
                     <img src=\"img/{$value['img']}\" alt=\"No Image\" style=\"height:50px; width:50px; object-fit: cover; object-position: center;\" />
                     <span class=\"item-info\" style=\"width:140px;\">
-                        <span>{$value['name']}</span>
-                        <span>\${$value['price']} x {$value['quantity']}</span>
-                    </span>
-                </span>
-                <span class=\"item-right\">
-                    <a href=\"\"><img src=\"img/delete.png\" style=\"max-height:25px;\" onclick=\"delFromCart($key)\"></a>
+                        <span>{$value['name']}</span>";
+        if (isset($_COOKIE['user'])) {
+            echo "<span><del>$$price</del> <strong class=\"text-danger\">$$vipPrice</strong> x {$value['quantity']}</span>";
+        } else {
+            echo "<span>\${$value['price']} x {$value['quantity']}</span>";
+        }        
+        echo "</span>
+            </span>
+            <span class=\"item-right\">
+                <a href=\"\"><img src=\"img/delete.png\" style=\"max-height:25px;\" onclick=\"delFromCart($key)\"></a>
                 </span>
             </span>
         </li>";
--- php/cartDetail.php
+++ php/cartDetail.php
@@ -18,14 +18,21 @@ if (isset($_SESSION['cart_details']) && count($_SESSION['cart_details'])>0) {
     $total = 0;
     foreach ($_SESSION['cart_details'] as $key => $value) {
         $unit = $value['price'];
+        $vipUnit = $unit * 0.9;
         $quantity = $value['quantity'];
         $price = $unit * $quantity;
+        $vipPrice = $price * 0.9;
         $total += $price;
+        $vipTotal += $vipPrice;
         echo "<tr>
                 <td class=\"align-middle\"><img src=\"img/{$value['img']}\" style=\"height:70px; width:70px; object-fit: cover; object-position: center;\"></td>
-                <td class=\"align-middle\">{$value['name']}</td>
-                <td class=\"align-middle text-xs-center\">$$unit</td>
-                <td class=\"align-middle text-xs-center\">
+                <td class=\"align-middle\">{$value['name']}</td>";
+        if (isset($_COOKIE['user'])) {
+            echo "<td class=\"align-middle text-xs-center\"><del>$$unit</del> <strong class=\"text-danger\">$$vipUnit</strong></td>";
+        } else {
+            echo "<td class=\"align-middle text-xs-center\">$$unit</td>";
+        }    
+        echo "<td class=\"align-middle text-xs-center\">
                 <div class=\"row\">
                     <div class=\"input-group\" style=\"width: 150px; margin:auto;\">
                       <span class=\"input-group-btn\">
@@ -37,15 +44,27 @@ if (isset($_SESSION['cart_details']) && count($_SESSION['cart_details'])>0) {
                       </span>
                     </div>
                 </div>
-                </td>
-                <th class=\"align-middle text-xs-center\" scope=\"row\">$$price</th>
-                <td class=\"align-middle text-xs-center\"><a href=\"\"><img src=\"img/delete.png\" style=\"max-height:50px;\" onclick=\"delFromCart($key)\"></a></td>
+                </td>";
+        if (isset($_COOKIE['user'])) {
+            echo "<th class=\"align-middle text-xs-center text-danger\" scope=\"row\">$$vipPrice</th>";
+        } else {
+            echo "<th class=\"align-middle text-xs-center\" scope=\"row\">$$price</th>";
+        }           
+        echo "<td class=\"align-middle text-xs-center\"><a href=\"\"><img src=\"img/delete.png\" style=\"max-height:50px;\" onclick=\"delFromCart($key)\"></a></td>
             </tr>";
     }
-    echo "<th colspan=\"5\" scope=\"row\" class=\"align-middle text-xs-right table-info h3\">Total Price: $$total</th>
-            <td class=\"align-middle table-info text-xs-right\"><button class=\"btn btn-primary btn-block\">Checkout</button></td>
-            </tbody>
-            </table>
-        </div>";
+    if (isset($_COOKIE['user'])) {
+        echo "<th colspan=\"5\" scope=\"row\" class=\"align-middle text-xs-right table-info h3\"><small class=\"text-muted\">Saved $".($total-$vipTotal)."</small> Total Price: $$vipTotal</th>
+                <td class=\"align-middle table-info text-xs-right\"><button class=\"btn btn-primary btn-block\">Checkout</button></td>
+                </tbody>
+                </table>
+            </div>";
+    } else {
+        echo "<th colspan=\"5\" scope=\"row\" class=\"align-middle text-xs-right table-info h3\">Total Price: $$total</th>
+                <td class=\"align-middle table-info text-xs-right\"><button class=\"btn btn-primary btn-block\">Checkout</button></td>
+                </tbody>
+                </table>
+            </div>";
+    }
 }
 ?>
